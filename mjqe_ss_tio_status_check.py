0cb799353a40b7d7fb126abc4878849fe3c61773
Diseirang <cseirang27@gmail.com>
2025-01-10 17:38:53 +0700

    update ss service

--- mjqe_ss_tio_status_check.py
+++ mjqe_ss_tio_status_check.py
@@ -1,24 +1,23 @@
 import os
 import requests
 from datetime import date , datetime
-import time
+import configparser
 
-# Configuration
-PC_IP = ["10.23.46.27", "10.23.46.26", "10.23.46.37","10.23.46.29"]  # Replace with your PC's IPs or hostnames
-MACHINE_NAME = ["Student Card 1","Student Card 2","Staff TimeIO"]
-LOCATIONS = ["Lobby","3rd Floor"]
-BUILDING = "Admin Building SenSok"
+# Load configuration
+config = configparser.ConfigParser()
+config.read("configss.properties")
 
-BOT_TOKEN = "7820861130:AAEkXmZlNsJrHxDrBWakH-XzeC893mYwHNQ"
-CHAT_ID = "-1002428026884"
-CHECK_INTERVAL = 60  # Check every 2 minutes
+BOT_TOKEN = config["DEFAULT"]["BOT_TOKEN"]
+CHAT_ID = config["DEFAULT"]["CHAT_ID"]
 
-DEVICE_NAME = None
-LOCATION = None
-MESSAGE = None
+# Parse host details from the configuration
+HOSTS = {key: value for key, value in config["HOSTS"].items() if key.lower() not in ["bot_token", "chat_id"]}
+PC_IP = list(HOSTS.values())
+HOST_NAMES = list(HOSTS.keys())
 
 # State tracking
-last_status = {ip: None for ip in PC_IP}  # Track the status of each IP
+last_status = {ip: None for ip in PC_IP}
+timeout_counter = {ip: 0 for ip in PC_IP}
 
 def send_telegram_notification(message):
     """Sends a notification to Telegram."""
@@ -36,47 +35,41 @@ def send_telegram_notification(message):
 def is_device_online(ip):
     """Checks if a device is online."""
     try:
-        # Ping the device
         response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1" if os.name != "nt" else f"ping -n 1 {ip} > nul")
         return response == 0
     except Exception as e:
         print(f"Error pinging {ip}: {e}")
         return False
     
-def check_device_name(PC_IP, MACHINE_NAME, LOCATIONS, ip):
-    if ip == PC_IP[0] :
-        DEVICE_NAME = MACHINE_NAME[0]
-        LOCATION= LOCATIONS[0]
-            
-    elif ip == PC_IP[1] :
-        DEVICE_NAME = MACHINE_NAME[1]
-        LOCATION= LOCATIONS[0]
-            
-    elif ip == PC_IP[2] :
-        DEVICE_NAME = MACHINE_NAME[2]
-        LOCATION= LOCATIONS[0]
-            
-    elif ip == PC_IP[3] :
-        DEVICE_NAME = MACHINE_NAME[2]
-        LOCATION= LOCATIONS[1]
-        
-    return DEVICE_NAME,LOCATION
-
+def get_device_info(ip):
+    """Returns the host name for the given IP."""
+    try:
+        index = PC_IP.index(ip)
+        return HOST_NAMES[index]
+    except ValueError:
+        return "Unknown Host"
+    
 while True:
     for ip in PC_IP:
         online_status = is_device_online(ip)
                 
-        DEVICE_NAME, LOCATION = check_device_name(PC_IP, MACHINE_NAME, LOCATIONS, ip) 
+        DEVICE_LOCATION = get_device_info(ip).upper()
 
-        CURRENT_DATE =  date.today()
-        CURRENT_TIME =  datetime.now()
+        CURRENT_DATE =  date.today().strftime('%B %d, %Y')
+        CURRENT_TIME =  datetime.now().strftime('%H:%M:%S %p')
 
-        if online_status != last_status[ip]:  # Send notification only if status changes
-            MESSAGE = f"🚨Staff&Student TimeIO Alert🚨\n\nDevice name: {DEVICE_NAME}\nLocation: {LOCATION}\nBuilding: {BUILDING}\nDate: {CURRENT_DATE.strftime('%B %d, %Y') }\nTime: { CURRENT_TIME.strftime('%H:%M:%S %p')}"
-            if online_status:
+        if online_status:
+            if timeout_counter[ip] > 0:
+                timeout_counter[ip] = 0
+
+            if online_status != last_status[ip]:
+                MESSAGE = f"🚨TimeIO Notification Alert🚨\n\nLocation: {DEVICE_LOCATION}\nIP: {ip}\nDate: {CURRENT_DATE}\nTime: {CURRENT_TIME}"
                 send_telegram_notification(f"{MESSAGE}\nStatus: UP! 📶✅\n")
-            else:
-                send_telegram_notification(f"{MESSAGE}\nStatus: DOWN! ❌❌\n")
-            last_status[ip] = online_status  # Update the last known status for this IP
+                last_status[ip] = online_status        
+        else:
+            timeout_counter[ip] += 1
 
-    time.sleep(CHECK_INTERVAL)  # Wait before the next check
+            if timeout_counter[ip] == 5:
+                MESSAGE = f"🚨TimeIO Notification Alert🚨\n\nLocation: {DEVICE_LOCATION}\nIP: {ip}\nDate: {CURRENT_DATE}\nTime: {CURRENT_TIME}"
+                send_telegram_notification(f"{MESSAGE}\nStatus: DOWN! ❌❌\n")
+                last_status[ip] = online_status
